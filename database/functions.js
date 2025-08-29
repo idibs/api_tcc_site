b5cf56b4cf364d64fd847741746b8c99fd2a333a
idibs <tiagosimoesprado@gmail.com>
2025-08-29 12:20:39 -0300

    arrumando codigo

--- database/functions.js
+++ database/functions.js
@@ -3,9 +3,27 @@ import connection from "./connection.js";
 export async function getUsers() {
   const conn = connection();
   return new Promise((resolve, reject) => {
-    conn.query(`SELECT Id_cli, Nome_cli, Email_cli, Senha_cli, Logradouro_end, Numero_end, 
-      Bairro_end, Cep_end, Complemento_end FROM Cliente
-        INNER JOIN Endereco ON Cliente.Id_end = Endereco.Id_end `, (error, results) => {
+    conn.query(
+      `SELECT Id_cli, Nome_cli, Email_cli, Senha_cli, Logradouro_end, Numero_end, Bairro_end, Cep_end, Complemento_end 
+      FROM Cliente
+      INNER JOIN Endereco ON Cliente.Id_end = Endereco.Id_end`,
+      (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results);
+        }
+      }
+    );
+    conn.end();
+  });
+}
+
+export async function insertUser(data) {
+  const conn = connection();
+  const query = `INSERT INTO Cliente (Nome_cli, Email_cli, Senha_cli, Id_end) VALUES (?, ?, ?, ?)`;
+  return new Promise((resolve, reject) => {
+    conn.query(query, data, (error, results) => {
       if (error) {
         reject(error);
       } else {
@@ -16,32 +34,35 @@ export async function getUsers() {
   });
 }
 
-export async function insertUser(data) {
+export async function insertAddress(data) {
   const conn = connection();
-    const query = `INSERT INTO Cliente (Nome_cli, Email_cli, Senha_cli, Id_end) VALUES (?, ?, ?, ?)`;
-    conn.query(query, data)
-    conn.end();
-}
-
-export async function insertAdress(data) {
-  const conn = connection();
-    const query = `INSERT INTO Endereco 
-    (Logradouro_end, Numero_end, Bairro_end, Cep_end, Complemento_end) 
-    VALUES (?, ?, ?, ?, ?)`;
-    conn.query(query, data);
-    conn.end();
-}
-
-export async function getAdressId(Logradouro_end) {
-  const conn = connection();
+  const query = `INSERT INTO Endereco (Logradouro_end, Numero_end, Bairro_end, Cep_end, Complemento_end) VALUES (?, ?, ?, ?, ?)`;
   return new Promise((resolve, reject) => {
-    conn.query(`SELECT Id_end FROM endereco`, (error, results) => {
+    conn.query(query, data, (error, results) => {
       if (error) {
         reject(error);
       } else {
         resolve(results);
       }
     });
+    conn.end();
+  });
+}
+
+export async function getAddressId(Logradouro_end) {
+  const conn = connection();
+  return new Promise((resolve, reject) => {
+    conn.query(
+      `SELECT Id_end FROM Endereco WHERE Logradouro_end = ?`,
+      [Logradouro_end],
+      (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results[0]?.Id_end);
+        }
+      }
+    );
     conn.end();
   });
 }
--- routes/public.js
+++ routes/public.js
@@ -1,31 +1,46 @@
 import express from "express";
-import { getUsers, insertUser, insertAdress, getAdressId } from "../database/functions.js";
+import { getUsers, insertUser, insertAddress, getAddressId } from "../database/functions.js";
 
 const router = express.Router();
 
 router.get("/cadastro", async (req, res) => {
-  const data = await getUsers();
-  const Id = await getAdressId()
-  res.send(data);
+  try {
+    const data = await getUsers();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
 });
 
 router.post("/cadastro", async (req, res) => {
-  const data = req.body;
-  const user = [
-    data.Nome_cli, 
-    data.Email_cli, 
-    data.Senha_cli
-  ]
-  const adress = [
-    data.Logradouro_end, 
-    data.Numero_end, 
-    data.Bairro_end, 
-    data.Cep_end, 
-    data.Complemento_end]
-  await insertAdress(adress)
-  const Id_end = await getAdressId(data.Logradouro_end)
-  user.push(Id_end)
-  await insertUser(user)
+  try {
+    const data = req.body;
+
+    const address = [
+      data.Logradouro_end,
+      data.Numero_end,
+      data.Bairro_end,
+      data.Cep_end,
+      data.Complemento_end,
+    ];
+
+    await insertAddress(address);
+
+    const Id_end = await getAddressId(data.Logradouro_end);
+
+    const user = [
+      data.Nome_cli,
+      data.Email_cli,
+      data.Senha_cli,
+      Id_end,
+    ];
+
+    await insertUser(user);
+
+    res.send({ message: "Usuário e endereço cadastrados com sucesso!" });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
 });
 
 export default router;
