5cd7d519c56eec3131a5f59a16ce827ffff13a41
idibs <tiagosimoesprado@gmail.com>
2025-10-02 21:48:32 -0300

    varios produtos em um pedido

--- routes/public.js
+++ routes/public.js
@@ -181,62 +181,64 @@ router.post("/pedido", async (req, res) => {
   try {
     const request = req.body;
 
-    const endereco = [
-      request.logradouro,
-      request.numero,
-      request.bairro,
-      request.cep,
-      request.complemento,
-    ];
+    for (const element of request) {
+      const endereco = [
+        element.logradouro,
+        element.numero,
+        element.bairro,
+        element.cep,
+        element.complemento,
+      ];
 
-    // Recuperando o ID do endereço recém-criado
-    let Id_end = await getEndereco(request.cep, request.numero);
+      let Id_end = await getEndereco(element.cep, element.numero);
 
-    if (!Id_end || Id_end.length === 0) {
-      await createEndereco(endereco);
-      Id_end = await getEndereco(request.cep, request.numero);
       if (!Id_end || Id_end.length === 0) {
-        return res.status(500).send("Erro ao recuperar o endereço");
+        await createEndereco(endereco);
+        Id_end = await getEndereco(element.cep, element.numero);
+        if (!Id_end || Id_end.length === 0) {
+          return res.status(500).send("Erro ao recuperar o endereço");
+        }
       }
-    }
 
-    const Id_pes = await getClienteIdByNome(request.nome);
+      const Id_pes = await getClienteIdByNome(element.nome);
 
-    if (!Id_pes || Id_pes.length === 0) {
-      return res.status(404).send("Cliente não encontrado");
-    }
+      if (!Id_pes || Id_pes.length === 0) {
+        return res.status(404).send("Cliente não encontrado");
+      }
 
-    let Id_prod = await getCerealIdByNome(request.produto);
+      let Id_prod = await getCerealIdByNome(element.produto);
 
-    if (!Id_prod || Id_prod.length === 0) {
-      Id_prod = await getOutrosProdutosIdByNome(request.produto);
       if (!Id_prod || Id_prod.length === 0) {
-        return res.status(404).send("Produto não encontrado");
+        Id_prod = await getOutrosProdutosIdByNome(element.produto);
+        if (!Id_prod || Id_prod.length === 0) {
+          return res.status(404).send("Produto não encontrado");
+        }
       }
+
+      const dataAtual = new Date();
+      const dataFormatada = dataAtual.toISOString().split("T")[0];
+      const valor_total = element.preco * element.quantidade;
+      const peso_total = Id_prod[0].Peso * element.quantidade;
+
+      const pedido = [
+        Id_pes[0].Id_pes,
+        Id_prod[0].Id_ens,
+        Id_prod[0].Id_out,
+        Id_end[0].Id_end,
+        dataFormatada,
+        element.quantidade,
+        peso_total,
+        valor_total,
+        element.metodo_pagamento,
+      ];
+
+      await createPedido(pedido);
     }
-
-    const dataAtual = new Date();
-    const dataFormatada = dataAtual.toISOString().split("T")[0]; // Formato YYYY-MM-DD
-    const valor_total = request.preco * request.quantidade;
-    const peso_total = Id_prod[0].Peso * request.quantidade;
-
-    const pedido = [
-      Id_pes[0].Id_pes,
-      Id_prod[0].Id_ens,
-      Id_prod[0].Id_out,
-      Id_end[0].Id_end,
-      dataFormatada,
-      request.quantidade,
-      peso_total,
-      valor_total,
-      request.metodo_pagamento,
-    ];
-
-    await createPedido(pedido);
 
     res.status(201).send("Pedido realizado com sucesso");
   } catch (error) {
     res.status(500).send({ error: error.message });
   }
 });
+
 export default router;
