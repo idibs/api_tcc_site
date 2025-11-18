aa06e951dbb9a53a6349507756bfb90c4b2ab8f4
idibs <tiagosimoesprado@gmail.com>
2025-11-18 08:14:05 -0300

    connctions no banco de produção

--- database/connection.js
+++ database/connection.js
@@ -2,9 +2,6 @@ import mysql from "mysql2";
 
 export default function connection() {
   return mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "tcc",
+    uri: "mysql://root:gPuDQcSHNzIAYSOyfMNBkeDOXcggxbwG@mysql.railway.internal:3306/railway"
   });
 }
