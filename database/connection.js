01476baeadf32eba5341ce2649bee33e1cc03b71
Gardelli07 <lucas.martins270@etec.sp.gov.br>
2025-11-23 01:04:50 -0300

    banco on

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,12 @@
 import mysql from "mysql2";
 
-export default function connection() {
-  return mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "tcc",
-  });
-}
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: true },
+});
+
+export default pool;
